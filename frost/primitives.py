a3b755731a9647dc11ed080c13169da38496e84b
Benjamin Moran <benmoran@protonmail.com>
2019-09-05 18:29:30 +0900

    Checkbox appears correctly now.

--- frost/primitives.py
+++ frost/primitives.py
@@ -95,6 +95,55 @@ def create_top_bottom(x, y, width, height, color1, color2):
     return verts, colors
 
 
+def _create_left(x, y, width, height, color1, color2):
+    one = x, y
+    two = x, y + height
+    three = x + width, y + height - width
+    four = x + width, y + width
+    verts = one + two + three + three + one + four
+    colors = color1 + color1 + color2 + color2 + color1 + color2
+    return verts, colors
+
+
+def _create_right(x, y, width, height, color1, color2):
+    one = x, y + width
+    two = x, y + height - width
+    three = x + width, y + height
+    four = x + width, y
+    verts = one + two + three + three + one + four
+    colors = color1 + color1 + color2 + color2 + color1 + color2
+    return verts, colors
+
+
+def _create_bottom(x, y, width, height, color1, color2):
+    one = x, y
+    two = x + width, y
+    three = x + width - height, y + height
+    four = x + height, y + height
+    verts = one + two + three + three + one + four
+    colors = color1 + color1 + color2 + color2 + color1 + color2
+    return verts, colors
+
+
+def _create_top(x, y, width, height, color1, color2):
+    one = x + height, y
+    two = x + width - height, y
+    three = x + width, y + height
+    four = x, y + height
+    verts = one + two + three + three + one + four
+    colors = color1 + color1 + color2 + color2 + color1 + color2
+    return verts, colors
+
+
+def _create_center(x, y, width, height, color):
+    one = x, y
+    two = x + width, y
+    three = x + width, y + height
+    four = x, y + height
+    verts = one + two + three + three + one + four
+    colors = color * 6
+    return verts, colors
+
 # def calculate_bottom(x, y, width, height, border, color1,  color2):
 #     b = border
 #     verts = [x, y,  x + width, y,  x + width - b, y + height,
@@ -159,12 +208,13 @@ def checkbox(x, y, width, height, border, color1=(25, 25, 25), color2=(50, 50, 5
     h = height
     b = border
 
-    # TODO: blend corners
+    bottom, bottom_c = _create_bottom(x, y, width, border, color1, color2)
+    left, left_c = _create_left(x, y, border, height, color1, color2)
+    right, right_c = _create_right(x + width - b, y, border, height, color2, color1)
+    top, top_c = _create_top(x, y + h - b, width, border, color2, color1)
 
-    bottom, bottom_c = create_top_bottom(x, y, width, border, color1, color2)
-    left, left_c = create_left_right(x, y, border, height, color1, color2)
-    right, right_c = create_left_right(x+width-b, y, border, height, color2, color1)
-    top, top_c = create_top_bottom(x, y+h-b, width, border, color2, color1)
+    # TODO: make center segment. Not working yet.
+    # center, center_c = _create_center(x + b, y + b, w - b, height - b, color=(0, 255, 0))
 
     verts = bottom + left + right + top
     colors = bottom_c + left_c + right_c + top_c
